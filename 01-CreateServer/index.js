98b89db1e2f60e62998d4d0c59a732dc9c869bec
Mayur <mayurkanade59@gmail.com>
2024-07-19 21:11:03 +0530

    http server creation using normal function

--- 01-CreateServer/index.js
+++ 01-CreateServer/index.js
@@ -1,8 +1,8 @@
 const http = require("http");
 
-http
-  .createServer((req, res) => {
-    res.write("<h1> The Server has been created </h1>");
-    res.end();
-  })
-  .listen(4500);
+function dataControl(req, res) {
+  res.write("<h1>The Server has been created. using Normal Function.</h1>");
+  res.end();
+}
+
+http.createServer(dataControl).listen(4500);
