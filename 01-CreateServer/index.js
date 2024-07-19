e29c8f0c4eee390bed105d750c8982e3d1796c1a
Mayur <mayurkanade59@gmail.com>
2024-07-19 21:13:03 +0530

    http server creation using Arrow function

--- 01-CreateServer/index.js
+++ 01-CreateServer/index.js
@@ -1,8 +1,8 @@
 const http = require("http");
 
-function dataControl(req, res) {
-  res.write("<h1>The Server has been created. using Normal Function.</h1>");
+const dataControl = (req, res) => {
+  res.write("<h1>The Server has been created. using Arrow Function.</h1>");
   res.end();
-}
+};
 
 http.createServer(dataControl).listen(4500);
